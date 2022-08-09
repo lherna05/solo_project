85264eee129d2a90a46f0e80c79e4df5f162ce0f
leilani <lherna05@gmail.com>
2022-08-09 14:37:28 -0700

    lowercased name

---
+++ CLIENT/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './stylesheets/styles.css';
+
+const App = props => {
+    return (
+        <div className="router">
+        <main>
+          < Main />
+        </main>
+      </div>
+    )
+}
