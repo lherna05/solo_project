56d8cedfbe261657c8f0bdc5419b977e02271e13
leilani <lherna05@gmail.com>
2022-08-09 15:31:03 -0700

    added server basics

---
+++ SERVER/server.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+
+//parse requesy body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//requests for static files 
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+//route handlers: 
+
+
+
+//catch-all route handler for requests to unknown routes 
+app.use((req, res) => res.status(404).send('The page you are requesting does not exist.')); 
+
+//global error 
+
+app.use((err, req, res, next) => {
+    const defaultError = {
+        log: 'Error handler caught unknown middleware error', 
+        status: 500, 
+        message: {err: 'An error has occured.'}
+    };
+    const errObj = Object.assign({}, defaultErr, err);
+    console.log(errObj);
+    return res.status(errObj.status).json(errObj.message);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}...`);
+}); 
+
+module.exports = app; 
